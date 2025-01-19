04702b76d47c0b410cbf347547a514dcc95e5166
Enzo <tsopanovenzo@gmail.com>
2025-01-19 17:32:14 +0300

    0.2.1 - доработка изображения

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ import sys
 WIDTH, HEIGHT = 500, 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+all_sprites = pygame.sprite.Group()
+pygame.init()
 
 
-class Bird:
+class Bird(pygame.sprite.Sprite):
+    bird_img = pygame.image.load('data/bird.png')
+    bird_img = pygame.transform.scale(bird_img, (44, 35))
+
     def __init__(self):
+        super().__init__(all_sprites)
         self.x = 150
         self.y = 300
-        self.width = 44
-        self.height = 35
-        self.bird_img = pygame.image.load('bird.png').convert_alpha()
-        self.bird_img = pygame.transform.scale(self.bird_img, (self.width, self.height))
         self.gravity = 0.5
         self.jump_strength = -9
         self.velocity_y = 0
@@ -24,8 +26,8 @@ class Bird:
     def update(self):
         self.velocity_y += self.gravity
         self.y += self.velocity_y
-        if self.y > HEIGHT - self.height:
-            self.y = HEIGHT - self.height
+        if self.y > HEIGHT - 35:
+            self.y = HEIGHT - 35
             self.velocity_y = 0
 
     def draw(self, screen):
@@ -33,7 +35,6 @@ class Bird:
 
 
 if __name__ == '__main__':
-    pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Flappy Bird")
     bird = Bird()
